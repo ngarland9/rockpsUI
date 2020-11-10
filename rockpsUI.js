9d7d04cb9e07620e56051d22eb9c9fec34db8957
ngarland9 <nateg@pvep.com>
2020-11-10 11:48:35 -0500

    changed img injection to a function. added img functions for tie game.

--- rockpsUI.js
+++ rockpsUI.js
@@ -14,7 +14,42 @@
         computerScore += 1;
       }
     }
-  
+    function rockPic() {
+      let elem = document.createElement("img");
+        elem.src = 'images/rock.png';
+        document.getElementById('playerIcon').innerHTML = ""
+        document.getElementById("playerIcon").appendChild(elem);
+    }
+    function paperPic() {
+      let elem = document.createElement("img");
+        elem.src = 'images/paper.png';
+        document.getElementById('playerIcon').innerHTML = ""
+        document.getElementById("playerIcon").appendChild(elem);
+    }
+    function scissorsPic() {
+      let elem = document.createElement("img");
+          elem.src = 'images/scissors.png';
+          document.getElementById('playerIcon').innerHTML = ""
+          document.getElementById("playerIcon").appendChild(elem);
+    }
+    function rockPicComp() {
+      let elem = document.createElement("img");
+        elem.src = 'images/rock.png';
+        document.getElementById('computerIcon').innerHTML = ""
+        document.getElementById("computerIcon").appendChild(elem);
+    }
+    function paperPicComp() {
+      let elem = document.createElement("img");
+        elem.src = 'images/paper.png';
+        document.getElementById('computerIcon').innerHTML = ""
+        document.getElementById("computerIcon").appendChild(elem);
+    }
+    function scissorsPicComp() {
+      let elem = document.createElement("img");
+          elem.src = 'images/scissors.png';
+          document.getElementById('computerIcon').innerHTML = ""
+          document.getElementById("computerIcon").appendChild(elem);
+    }
     function computerPlay() {
       return options[(Math.floor(Math.random() * 3))]
     }
@@ -27,47 +62,56 @@
 
     function playRound(playerSelection, computerSelection) {
       if (playerSelection === computerSelection) {
-        return "It's a tie!! Play again! "
+       if (playerSelection == "rock") {
+         rockPic();
+         rockPicComp();
+       }
+       else if (playerSelection == "paper") {
+         paperPic();
+         paperPicComp();
+       }
+       else if (playerSelection == "scissors") {
+         scissorsPic();
+         scissorsPicComp();
+       }
+      return "It's a tie!! Play again! ";
       }
       if (playerSelection == "rock") {
-        let elem = document.createElement("img");
-        elem.src = 'images/rock.png';
-        document.getElementById('playerIcon').innerHTML = ""
-        document.getElementById("playerIcon").appendChild(elem);
+          rockPic();
         if (computerSelection == "paper") {
           score();
-          let elem = document.createElement("img");
-          elem.src = 'images/paper.png';
-          document.getElementById('computerIcon').innerHTML = ""
-          document.getElementById("computerIcon").appendChild(elem);
+          paperPicComp();
           return "You lose! Paper beats Rock! ";
         }
         else {
           score("player");
-          let elem = document.createElement("img");
-          elem.src = 'images/scissors.png';
-          document.getElementById('computerIcon').innerHTML = ""
-          document.getElementById("computerIcon").appendChild(elem);
+          scissorsPicComp();
           return "You win! Rock beats Scissors! ";
         }
       }
       if (playerSelection == "paper") {
+        paperPic();
         if (computerSelection == "scissors") {
           score();
+          scissorsPicComp();
           return "You lose! Scissors beats Paper! ";
         }
         else {
           score("player");
+          rockPicComp();
           return "You win! Paper beats Rock! ";
         }
       }
         if (playerSelection == "scissors") {
+          scissorsPic();
           if (computerSelection == "rock") {
             score();
+            rockPicComp();
             return "You lose! Rock beats Scissors! ";
           }
           else {
             score("player");
+            paperPicComp();
             return "You win! Scissors beats paper! ";
           }
         }
